b103a4bf2585e47156bac65f8456ec1e89ae7a62
itsAbhayRaj <ay0218757@gmail.com>
2025-09-22 14:26:35 +0530

    bug fixes

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-import os
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare.settings.prod')
-
-# Get the WSGI application
-application = get_wsgi_application()
